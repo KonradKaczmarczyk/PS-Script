bfd826b1834340e4ad051a4f2cfdd3e81958650b
KonradKaczmarczyk <103384314+KonradKaczmarczyk@users.noreply.github.com>
2024-03-29 13:13:52 +0100

    Add files via upload

---
+++ CheckingMBRforCentreon.ps1
@@ -0,0 +1,33 @@
+$disksObject = @()
+$mbrVolumesCount = (Get-Disk | Where-Object -FilterScript {$_.PartitionStyle -Eq "MBR"} | Measure-Object).Count
+Get-WmiObject Win32_Volume -Filter "DriveType='3'" | ForEach-Object {
+    $VolObj = $_
+    $ParObj = Get-Partition | Where-Object { $_.AccessPaths -contains $VolObj.DeviceID } | Where-Object -FilterScript {$_.Type -Eq "IFS"}
+    if ( $ParObj ) {
+        $disksobject += [pscustomobject][ordered]@{
+            DriveLetter = $VolObj.DriveLetter
+            Name = $VolObj.Label
+            Size = ([Math]::Round(($VolObj.Capacity / 1GB),2))
+            Text = "This is MBR"
+            }
+        }
+    }
+
+Write-Host Count of MBR volumes: $mbrVolumesCount
+$disksObject | Sort-Object DriveLetter | Format-Table -AutoSize
+
+$Disks = Get-Disk | Where-Object -FilterScript {($_.Size/1GB)}
+    $FindDisk = $false
+    foreach ($Disk in $Disks) {
+    $SizeGB = [math]::Round($Disk.Size/1GB,2)
+
+    if ($Disk.PartitionStyle -eq 'MBR' -and $SizeGB -gt 150) {
+        Write-Host "tutaj"
+        $FindDisk = $true
+        exit 1
+    }
+    elseif (-not $FindDisk){
+    }
+    }
+    Write-Host 'to nie ten'   
+    exit 0
